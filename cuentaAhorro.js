1bebd890c3444799a23a2b4b67041c02dfff4f8b
Tessera04 <matute_11@hotmail.com.ar>
2022-11-19 01:00:40 -0300

    Herencia

--- cuentaAhorro.js
+++ cuentaAhorro.js
@@ -1,34 +1,7 @@
-export class cuentaAhorro {
-    #cliente;
-    #saldo;
+import { Cuenta } from "./Cuenta.js";
 
-    constructor(cliente, numero, agencia, saldo){
-        this.numero = numero;
-        this.agencia = agencia;
-        this.#cliente = cliente;
-        this.#saldo = saldo;
-    }
-
-    depositoEnCuenta(valor) {
-        if (valor > 0)
-            this.#saldo += valor;
-        return this.#saldo;
-    }
-
-    retirarDeCuenta(valor) {
-        if (valor <= this.#saldo)
-            this.#saldo -= valor;
-        return this.#saldo;
-    }
-
-    verSaldo() {
-        return this.#saldo;
-    }
-
-    transferirParaCuenta(valor,cuentaDestino) {
-        this.retirarDeCuenta(valor);
-        cuentaDestino.depositoEnCuenta(valor);
-        valor = 200;
-        valor = valor*1000;
+export class cuentaAhorro extends Cuenta{
+    constructor(cliente, numero, agencia, saldo) {
+        super(cliente, numero, agencia, saldo);
     }
 }
--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 /*Importación de clases*/
 import {Cliente} from './Cliente.js'
-//import {CuentaCorriente} from './CuentaCorriente.js';
-//import {cuentaAhorro} from './cuentaAhorro.js';
-import {Cuenta} from './Cuenta.js';
+import { cuentaAhorro } from './cuentaAhorro.js';
+import { CuentaCorriente } from './CuentaCorriente.js';
 
 const cliente = new Cliente('Leonardo','13804050','123224');
 const cliente2 = new Cliente('María','16979808','8989');
 
-const cuentaDeLeonardo = new Cuenta('Corriente', cliente, '1', '001', 0);
-const cuentaDeMaria = new Cuenta('Corriente', cliente2, '2', '002', 0);
+const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
+const cuentaDeMaria = new CuentaCorriente(cliente2, '2', '002');
 
-const cuentaAhorroLeonardo = new Cuenta('Ahorro', cliente, '9985', '001', 0);
+const cuentaAhorroLeonardo = new cuentaAhorro(cliente, '9985', '001');
 
 console.log(cuentaDeLeonardo);
 
 cuentaDeLeonardo.depositoEnCuenta(1500);
 console.log(cuentaDeLeonardo.verSaldo());
-
-console.log(cuentaDeLeonardo);
-
 cuentaDeLeonardo.retirarDeCuenta(500);
 console.log(cuentaDeLeonardo.verSaldo());
 
