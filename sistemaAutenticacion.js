2ef5bb4aab5ba50c3e6b74a23744d9469ce81010
Tessera04 <matute_11@hotmail.com.ar>
2022-11-20 18:08:47 -0300

    interfaces de autenticacion

--- sistemaAutenticacion.js
+++ sistemaAutenticacion.js
@@ -1,5 +1,6 @@
 export class SistemaAutenticacion {
     static login(usuario, clave){
-        return usuario.clave == clave;
+        return usuario.autenticable(clave);
+        //return usuario.clave == clave;
     }
 }
